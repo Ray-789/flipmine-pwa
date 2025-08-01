61f5bf71d3e827dad5156f85151e73736bfad445
Ray-789 <saidanirayen961@gmail.com>
2025-08-01 16:18:42 -0700

    "commitage"

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -8,7 +8,7 @@ export default function manifest(): MetadataRoute.Manifest {
     start_url: "/",
     display: "standalone",
     background_color: "#ffffff",
-    theme_color: "#1976d2",
+    theme_color: "#030712",
     icons: [
         {
             src: "./1.png",
